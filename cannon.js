4ef8352c63507eec55b4da1ade403a97101ce76a
Annie <atc16@sfu.ca>
2024-10-23 12:59:37 -0700

    make turret goes up and down

--- cannon.js
+++ cannon.js
@@ -19,6 +19,24 @@ var myGameArea = {
             // Handle the resizing logic
             myGamePiece.handleResize();
         });
+
+        window.addEventListener('keydown', (e) => {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        });
+
+        window.addEventListener('keyup', (e) => {
+            myGameArea.keys[e.keyCode] = false;
+        });
+
+        this.interval = setInterval(this.update, 20); // Update the game area every 20ms
+    },
+    update: function() {
+        myGameArea.clear();
+        myGamePiece.update();
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -29,7 +47,7 @@ function component() {
     // Set initial position (bottom-left)
     this.updatePosition = function() {
         this.x = 0; // Set to left edge of the screen
-        this.y = myGameArea.canvas.height - 12  0; // Set to bottom of the canvas, assuming sprite height is 100
+        this.y = myGameArea.canvas.height - 120; // Set to bottom of the canvas, assuming sprite height is 100
     };
 
     this.updatePosition(); // Initialize the position
@@ -40,7 +58,7 @@ function component() {
     // Array for sprite images (layers)
     this.images = [
         { src: 'img/Carriage.png', width: 200, x: this.x, y: this.y, image: new Image() },
-        { src: 'img/Turret.png', width: 250, x: this.x, y: this.y - 40, image: new Image() },
+        { src: 'img/Turret.png', width: 250, x: this.x, y: this.y - 40, image: new Image(), angle: 0 },
         { src: 'img/Wheel.png', width: 120, x: this.x + 80, y: this.y - 40, image: new Image() }
     ];
 
@@ -76,6 +94,18 @@ function component() {
         this.redraw(); // Redraw the sprite with the updated position
     };
 
+    // Update the turret angle based on key presses
+    this.update = () => {
+        if (myGameArea.keys && myGameArea.keys[38]) { this.images[1].angle -= 1; } // Up arrow key
+        if (myGameArea.keys && myGameArea.keys[40]) { this.images[1].angle += 1; } // Down arrow key
+
+        // Restrict the turret angle
+        if (this.images[1].angle < -20) { this.images[1].angle = -20; }
+        if (this.images[1].angle > 35) { this.images[1].angle = 35; }
+
+        this.redraw();
+    };
+
     // Redraw the sprite on the canvas
     this.redraw = () => {
         const ctx = myGameArea.context;
@@ -85,7 +115,17 @@ function component() {
         this.images.forEach((layer) => {
             const aspectRatio = layer.image.height / layer.image.width;
             const height = layer.width * aspectRatio;
-            ctx.drawImage(layer.image, layer.x, layer.y, layer.width, height);
+            if (layer.src === 'img/Turret.png') {
+                ctx.save();
+                ctx.translate(layer.x + layer.width / 2, layer.y + height / 2);
+                ctx.rotate(layer.angle * Math.PI / 180);
+                ctx.drawImage(layer.image, -layer.width / 2, -height / 2, layer.width, height);
+                ctx.restore();
+            } else {
+                ctx.drawImage(layer.image, layer.x, layer.y, layer.width, height);
+            }
         });
     };
 }
+
+startGame();
